cdc6fac317eebf239adaaf81877c155ab9936d68
PauG <paugaston@hotmail.com>
2025-10-16 12:40:16 +0200

    Level 1, exercise 2, 3 and 4 completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -7,20 +7,38 @@ public class Main {
     static void main(String[] args) {
 
         IO.println("Demonstrating Exercise 1:\n");
-        List<String> stringList = new ArrayList<>();
-        stringList.add("Rotulador");
-        stringList.add("Tungsteno");
-        stringList.add("Zarzaparrilla");
-        stringList.add("Transfuncionador del continuo");
-        stringList.add("Ni cintieni la i");
+        List<String> originalList = new ArrayList<>();
+        originalList.add("Rotulador");
+        originalList.add("Tungsteno");
+        originalList.add("Zarzaparrilla");
+        originalList.add("Transfuncionador del continuo");
+        originalList.add("Ni cintieni la i");
+        originalList.add("Tonto");
 
         IO.println("Printing original List.");
-        stringList.forEach(IO::println);
+        originalList.forEach(IO::println);
         IO.println("");
         IO.println("Printing a list with strings that has the letter 'o':");
 
-        stringList = MethodsWithLamba.filterStringsWithO(stringList);
-        stringList.forEach(IO::println);
+        List<String> filteredList = new ArrayList<>();
+        filteredList = MethodsWithLamba.filterStringsWithO(originalList);
+        filteredList.forEach(IO::println);
+
+        IO.println("");
+        IO.println("Demonstrating Exercise 2:");
+        filteredList = MethodsWithLamba.filterStringsWithOandLength5(originalList);
+        filteredList.forEach(IO::println);
+
+        IO.println("");
+        IO.println("Demonstrating Exercise 3:");
+
+        List<String> months = new ArrayList<>(Arrays.asList("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"));
+        months.forEach(s->IO.println(s));
+
+        IO.println("");
+        IO.println("Demonstrating Exercise 4:");
+        months.forEach(IO::println);
+
 
 
 
--- src/main/java/MethodsWithLamba.java
+++ src/main/java/MethodsWithLamba.java
@@ -1,10 +1,14 @@
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 class MethodsWithLamba {
 
     public static List<String> filterStringsWithO (List<String> list){
         return list.stream().filter(s->s.toLowerCase().contains("o")).toList();
+    }
+
+    public static List<String> filterStringsWithOandLength5 (List<String> list){
+        return list.stream().filter(s->s.toLowerCase().contains("o") && s.length()==5).toList();
     }
 
 }
