4112c95e12f09f8c01b0465b24c98d1076b280e6
PauG <paugaston@hotmail.com>
2025-10-21 10:17:03 +0200

    including forgotten exercise from lvl3

--- src/main/java/level3/Main.java
+++ src/main/java/level3/Main.java
@@ -3,6 +3,7 @@ package level3;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 class Main {
 
@@ -14,7 +15,7 @@ class Main {
         std3 = new Student("Andrea",35,"JAVA",5.2);
         std4 = new Student("Yisus", 30, "JAVA", 9.9);
         std5 = new Student("Fernando", 58,"PHP",8.8);
-        std6 = new Student("Hector", 16,"PHP", 4.5);
+        std6 = new Student("Hector", 16,"JAVA", 4.5);
         std7 = new Student("Jose",18,"CIBERSEGURIDAD",4.8);
         std8 = new Student("Joan",32,"JAVA",5);
         std9 = new Student("Almudena",89,"PHP",8);
@@ -37,7 +38,12 @@ class Main {
         IO.println("");
 
         IO.println("Students with a grade of 5 or more that do not study PHP:");
-        studenstList.stream().filter(s->s.getGrades()>=5).filter(s->s.getCourse()!="PHP").forEach(s->IO.println(s.toString()));
+        studenstList.stream().filter(s->s.getGrades()>=5).filter(s-> !Objects.equals(s.getCourse(), "PHP")).forEach(s->IO.println(s.toString()));
+        IO.println("");
+
+        IO.println("Students with 18 yo or more that Study Java:");
+        studenstList.stream().filter(s->Objects.equals(s.getCourse(),"JAVA")).filter(s->s.getAge()>=18).forEach(s->IO.println(s.toString()));
+
 
     }
 
